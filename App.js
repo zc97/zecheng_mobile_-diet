7aba0dbb533c25ca47739af4998fa1e386ec23f3
zc97 <zecheng.chow@gmail.com>
2024-10-09 23:34:45 -0700

    added navigator stack for Activities and Diet Screens

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Activities from './screens/Activities';
+import Diet from './screens/Diet';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Activities" component={Activities} />
+        <Stack.Screen name="Diet" component={Diet} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
---
+++ screens/Diet.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Diet() {
+  return (
+    <View>
+      <Text>Diet</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
